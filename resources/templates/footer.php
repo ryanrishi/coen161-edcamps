6a2633ab55f241c34ab601492e8bbb45e984d6e8
Ryan Rishi <rdrishi12@gmail.com>
2016-03-07 11:33:24 -0800

    added templates

---
+++ resources/templates/footer.php
@@ -0,0 +1,17 @@
+<footer class="footer navbar-fixed-bottom">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-4">
+        Left footer
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4">
+        <p>Copyright &copy; <?php echo date("Y"); ?> EdCamps.
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-4">
+        right footer
+      </div>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
---
+++ resources/templates/head.php
@@ -0,0 +1,9 @@
+<?php
+// Put this inside <head> tag
+echo '<!-- styles -->';
+echo '<link href="/resources/lib/css/bootstrap.min.css" rel="stylesheet" type="text/css" />';
+
+echo '<!-- scripts -->';
+echo '<script src="/resources/lib/js/jquery-1.12.1.min.js" type="text/javascript"></script>';
+echo '<script src="/resources/lib/js/bootstrap.min.js" type="text/javascript"></script>';
+?>
