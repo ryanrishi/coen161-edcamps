9e1d3cd87a185d005647cf72a3a8dc3f0708d388
Ryan Rishi <rdrishi12@gmail.com>
2016-03-07 18:52:24 -0800

    added more dummy data

--- sql/populate_db.sql
+++ sql/populate_db.sql
@@ -2,3 +2,8 @@ INSERT INTO activities(name, description) VALUES ("Pottery", "Making pottery");
 INSERT INTO campsites(name, campsite_address_id, office_address_id, phone) VALUES ("MyCampsite", 1, 2, "1234567890");
 INSERT INTO addresses(address_1, city, state, zip) VALUES ('500 El Camino Real', 'Santa Clara', 'California', 95053);
 INSERT INTO addresses(address_1, city, state,  zip) VALUES ('1234 First Street', 'Santa Clara', 'California', 95050);
+INSERT INTO camp_sessions(campsite_id, start);
+
+INSERT INTO campsites(name, campsite_address_id, office_address_id, phone) VALUES ("Second Campsite", 1, 2, "2345678901");
+
+INSERT INTO camp_sessions(campsite_id, start_date, end_date) VALUES (1, "2016-06-01", "2016-06-08");
