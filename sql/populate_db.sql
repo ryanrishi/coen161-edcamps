9f37ee4390ea0a67c9af79bc221069ac4436250b
jaulakh <jess-ie1@hotmail.com>
2016-03-08 13:16:25 -0800

    Update populate_db.sql

--- sql/populate_db.sql
+++ sql/populate_db.sql
@@ -8,5 +8,9 @@ INSERT INTO campsites(name, campsite_address_id, office_address_id, phone) VALUE
 
 INSERT INTO camp_sessions(campsite_id, start_date, end_date) VALUES (1, "2016-06-01", "2016-06-08");
 
-INSERT INTO questions (question, answer) VALUES ("How many camp locations are there?", "There are 8 camp locations.");
+INSERT INTO questions (question, answer) VALUES ("How many camp locations are there?", "There are 5 camp locations.");
 INSERT INTO questions (question, answer) VALUES ("How many sessions are there?", "Each campsite location has between 4 and 8 camp sessions.");
+INSERT INTO questions (question, answer) VALUES ("What is the deadline for signing up for a session?", "You can sign up as long as there is space available.");
+INSERT INTO questions (question, answer) VALUES ("Are there vegetarian options for lunch?", "Yes your child may request a vegetarian option for lunch.");
+INSERT INTO questions (question, answer) VALUES ("What happens if the session I want is already full?", "You may join our waitlist for this session free of charge. Just call 800-222-3443."
+INSERT INTO questions (question, answer) VALUES ("Will I receive a refund if I cancel my child's enrollment?", "You will receive a full refund up to 2 weeks before the camp. After this, you will only receive a refund of $120."
