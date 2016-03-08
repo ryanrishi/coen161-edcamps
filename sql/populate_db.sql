c016b698aae55158a673661b9ce8224d2806d2c8
jaulakh <jess-ie1@hotmail.com>
2016-03-08 13:21:29 -0800

    Update populate_db.sql

--- sql/populate_db.sql
+++ sql/populate_db.sql
@@ -1,4 +1,10 @@
 INSERT INTO activities(name, description) VALUES ("Pottery", "Making pottery");
+INSERT INTO activities(name, description) VALUES ("3D Printing", "Print a 3D object of choice");
+INSERT INTO activities(name, description) VALUES ("Swimming", "Swim at nearby lakes and waterparks");
+INSERT INTO activities(name, description) VALUES ("Hiking", "Hike nearby peaks");
+INSERT INTO activities(name, description) VALUES ("Soccer", "Play soccer");
+INSERT INTO activities(name, description) VALUES ("Programming", "Learn basic coding languages");
+
 INSERT INTO campsites(name, campsite_address_id, office_address_id, phone) VALUES ("MyCampsite", 1, 2, "1234567890");
 INSERT INTO addresses(address_1, city, state, zip) VALUES ('500 El Camino Real', 'Santa Clara', 'California', 95053);
 INSERT INTO addresses(address_1, city, state,  zip) VALUES ('1234 First Street', 'Santa Clara', 'California', 95050);
