707c83f1aeb4e4bba5f01d9b86bbdde2bda8a2a4
Ryan Rishi <rdrishi12@gmail.com>
2016-03-08 12:51:40 -0800

    added jquery-ui

--- resources/templates/head.php
+++ resources/templates/head.php
@@ -2,8 +2,11 @@
 // Put this inside <head> tag
 echo '<!-- styles -->';
 echo '<link href="/resources/lib/css/bootstrap.min.css" rel="stylesheet" type="text/css" />';
+echo '<link href="/resources/lib/css/jquery-ui.min.css" rel="stylesheet" type="text/css" />';
+echo '<link href="/public/styles/style.css" rel="stylesheet" type="text/css" />';
 
 echo '<!-- scripts -->';
 echo '<script src="/resources/lib/js/jquery-1.12.1.min.js" type="text/javascript"></script>';
 echo '<script src="/resources/lib/js/bootstrap.min.js" type="text/javascript"></script>';
+echo '<script src="/resources/lib/js/jquery-ui.min.js" type="text/javascript"></script>';
 ?>
