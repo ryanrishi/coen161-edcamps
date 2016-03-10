3f0778f382cb1203e788cdbccf948921c5f4c412
Ryan Rishi <rdrishi12@gmail.com>
2016-03-10 01:22:44 -0800

    cleaned up FAQ

--- public/faq.php
+++ public/faq.php
@@ -4,6 +4,7 @@ require_once(TEMPLATES_PATH . "/header.php");
 ?>
 <div class="container container-fluid">
   <h1>Frequently Asked Questions</h1>
+  <hr>
   <?php
     require_once('helpers/db.php');
     $conn = get_db_conn();
@@ -14,12 +15,12 @@ require_once(TEMPLATES_PATH . "/header.php");
     while ($row = mysqli_fetch_assoc($result)) {
       $question = $row['question'];
       $answer = $row['answer'];
-      echo '<div>';
+      echo '<div class="question-answer-pair">';
       echo '<p class="question">' . $question . '</p>';
       echo '<p class="answer">' . $answer . '</p>';
+      echo '<hr>';
       echo '</div>';
     }
   ?>
-  <div class="question-answer-pair"></div>
 </div>
 <?php require_once(TEMPLATES_PATH . "/footer.php"); ?>
