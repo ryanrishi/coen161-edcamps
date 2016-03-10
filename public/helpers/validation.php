23c203582100c71fcf0bf13b60c1158b149bd5e2
Ryan Rishi <rdrishi12@gmail.com>
2016-03-10 01:25:22 -0800

    added server-side validation of emails and phone numbers

--- public/helpers/validation.php
+++ public/helpers/validation.php
@@ -1,11 +1,13 @@
 <?php
-function validate_email($email) {
-  // todo
-  return True;
+function validate_phone($phone) {
+  $phoneRe = '/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}\$/';
+  $match = preg_match($phoneRe, $phone);
+  return ($match === 1);
 }
 
-function validate_phone($phone) {
-  // todo
-  return True;
+function validate_email($email) {
+  $emailRe = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))\$/';
+  $match = preg_match($emailRe, $email);
+  return ($match === 1);
 }
 ?>
