2c71a5d2d7e9f1fcbab0f261fb690e1e77968aa5
Ryan Rishi <rdrishi12@gmail.com>
2016-03-10 01:23:02 -0800

    cleaned up login/logout process

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,29 @@
 $(document).ready(function() {
-  $('#login').click(function() {
-    console.debug('click');
-    $.ajax({
-      url: 'login.php',
-      method: 'POST',
-      success: function(d) {
-        console.debug(d);
-        $('.account-name').text(d.first_name + ' ' + d.last_name);
+  $('#login').click(function(evt) {
+    // dumb way to check if user is logged in
+    if ($('#login').html() === 'Login') {
+      $.ajax({
+        url: 'login.php',
+        method: 'POST',
+        success: function(d) {
+          console.debug(d);
+          $('.account-name').text(d.first_name + ' ' + d.last_name);
+        }
+      });
+    }
+    else {
+      // user is already logged in
+      // don't log out if they clicked caret next to login
+      if (evt.currentTarget.classList.contains('dropdown-toggle')) {
+        return;
       }
-    })
-  })
+      $.ajax({
+        url: 'logout.php',
+        method: 'POST',
+        success: function() {
+          $('.account-name').text('Login'); // set back to "Login"
+        }
+      });
+    }
+  });
 });
