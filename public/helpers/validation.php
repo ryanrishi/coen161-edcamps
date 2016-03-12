c20ea4c10119464273327e2d1335e1fb42f64e18
Ryan Rishi <rdrishi12@gmail.com>
2016-03-11 16:19:47 -0800

    added server-side validation

--- public/helpers/validation.php
+++ public/helpers/validation.php
@@ -1,13 +1,15 @@
 <?php
 function validate_phone($phone) {
-  $phoneRe = '/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}\$/';
-  $match = preg_match($phoneRe, $phone);
-  return ($match === 1);
+  echo $phone;
+  $phoneRe = '/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/';
+  preg_match($phoneRe, $phone, $matches);
+  var_dump($matches);
+  return (!empty($matches));
 }
 
 function validate_email($email) {
-  $emailRe = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))\$/';
-  $match = preg_match($emailRe, $email);
-  return ($match === 1);
+  $emailRe = '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
+  preg_match($emailRe, $email, $matches);
+  return (!empty($matches));
 }
 ?>
